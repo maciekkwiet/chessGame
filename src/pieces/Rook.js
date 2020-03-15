0a91f090b17d12480112a7fc19d235672f92072c
Juniper <agata5810@gmail.com>
2020-03-15 22:03:49 +0100

    rook - lewo - dół

--- src/pieces/Rook.js
+++ src/pieces/Rook.js
@@ -8,22 +8,37 @@ class Rook extends Piece {
   }
   findLegalMoves() {
     const possibleMoves = [];
-    this.y - 1 > 0 && possibleMoves.push(`${this.x},${this.y - 1}`);
-    this.y - 2 > 0 && possibleMoves.push(`${this.x},${this.y - 2}`);
-    this.y - 3 > 0 && possibleMoves.push(`${this.x},${this.y - 3}`);
-    this.y - 4 > 0 && possibleMoves.push(`${this.x},${this.y - 4}`);
-    this.y - 5 > 0 && possibleMoves.push(`${this.x},${this.y - 5}`);
-    this.y - 6 > 0 && possibleMoves.push(`${this.x},${this.y - 6}`);
-    this.y - 6 > 0 && possibleMoves.push(`${this.x},${this.y - 7}`);
+      this.y - 1 > 0 && possibleMoves.push(`${this.x},${this.y - 1}`);
+      this.y - 2 > 0 && possibleMoves.push(`${this.x},${this.y - 2}`);
+      this.y - 3 > 0 && possibleMoves.push(`${this.x},${this.y - 3}`);
+      this.y - 4 > 0 && possibleMoves.push(`${this.x},${this.y - 4}`);
+      this.y - 5 > 0 && possibleMoves.push(`${this.x},${this.y - 5}`);
+      this.y - 6 > 0 && possibleMoves.push(`${this.x},${this.y - 6}`);
+      this.y - 7 > 0 && possibleMoves.push(`${this.x},${this.y - 7}`);
 
-    this.x - 1 > 0 && possibleMoves.push(`${this.x - 1},${this.y}`);
-    this.x - 2 > 0 && possibleMoves.push(`${this.x - 2},${this.y}`);
-    this.x - 3 > 0 && possibleMoves.push(`${this.x - 3},${this.y}`);
-    this.x - 4 > 0 && possibleMoves.push(`${this.x - 4},${this.y}`);
-    this.x - 5 > 0 && possibleMoves.push(`${this.x - 5},${this.y}`);
-    this.x - 6 > 0 && possibleMoves.push(`${this.x - 6},${this.y}`);
-    this.x - 6 > 0 && possibleMoves.push(`${this.x - 7},${this.y}`);
+      this.x - 1 > 0 && possibleMoves.push(`${this.x - 1},${this.y}`);
+      this.x - 2 > 0 && possibleMoves.push(`${this.x - 2},${this.y}`);
+      this.x - 3 > 0 && possibleMoves.push(`${this.x - 3},${this.y}`);
+      this.x - 4 > 0 && possibleMoves.push(`${this.x - 4},${this.y}`);
+      this.x - 5 > 0 && possibleMoves.push(`${this.x - 5},${this.y}`);
+      this.x - 6 > 0 && possibleMoves.push(`${this.x - 6},${this.y}`);
+      this.x - 7 > 0 && possibleMoves.push(`${this.x - 7},${this.y}`);
+      
+      this.y - 1 < 0 && possibleMoves.push(`${this.x},${this.y - -1}`);
+      this.y - 2 < 0 && possibleMoves.push(`${this.x},${this.y - -2}`);
+      this.y - 3 < 0 && possibleMoves.push(`${this.x},${this.y - -3}`);
+      this.y - 4 < 0 && possibleMoves.push(`${this.x},${this.y - -4}`);
+      this.y - 5 < 0 && possibleMoves.push(`${this.x},${this.y - -5}`);
+      this.y - 6 < 0 && possibleMoves.push(`${this.x},${this.y - -6}`);
+      this.y - 7 < 0 && possibleMoves.push(`${this.x},${this.y - -7}`);
 
+      this.x - 1 < 0 && possibleMoves.push(`${this.x - -1},${this.y}`);
+      this.x - 2 < 0 && possibleMoves.push(`${this.x - -2},${this.y}`);
+      this.x - 3 < 0 && possibleMoves.push(`${this.x - -3},${this.y}`);
+      this.x - 4 < 0 && possibleMoves.push(`${this.x - -4},${this.y}`);
+      this.x - 5 < 0 && possibleMoves.push(`${this.x - -5},${this.y}`);
+      this.x - 6 < 0 && possibleMoves.push(`${this.x - -6},${this.y}`);
+      this.x - 7 < 0 && possibleMoves.push(`${this.x - -7},${this.y}`);
     return possibleMoves;
   }
 }
