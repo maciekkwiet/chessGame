7fb8c09f0027143b84ea76b3486663ad66d31468
wbrydak <brydakwojciech@gmail.com>
2020-03-18 17:42:07 +0100

    Minor fix Removed unnecessary 'console.log' statements and comments.

--- src/pieces/Pawn.js
+++ src/pieces/Pawn.js
@@ -8,52 +8,42 @@ class Pawn extends Piece {
   }
   findLegalMoves(board) {
     const possibleMoves = [];
-    console.log(possibleMoves);
 
     if (this.side == 'white') {
-      console.log(this.side);
       if (!board[this.x][this.y - 1]) {
         possibleMoves.push(`${this.x},${this.y - 1}`);
       }
-      console.log(possibleMoves);
 
       if (this.y == 6 && !board[this.x][this.y - 2]) {
         possibleMoves.push(`${this.x},${this.y - 2}`);
       }
-      console.log(possibleMoves);
       if (board[this.x][this.y - 1]) {
         if (this.x != 0 && board[this.x - 1][this.y - 1] && this.side != board[this.x - 1][this.y - 1].side) {
           possibleMoves.push(`${this.x - 1},${this.y - 1}`);
         }
-        console.log(possibleMoves);
 
         if (this.x != 7 && board[this.x + 1][this.y - 1] && this.side != board[this.x + 1][this.y - 1].side) {
           possibleMoves.push(`${this.x + 1},${this.y - 1}`);
         }
       }
-      console.log(possibleMoves);
     }
     if (this.side == 'black') {
       if (!board[this.x][this.y + 1]) {
         possibleMoves.push(`${this.x},${this.y + 1}`);
       }
-      console.log(possibleMoves);
 
       if (this.y == 1 && !board[this.x][this.y + 2]) {
         possibleMoves.push(`${this.x},${this.y + 2}`);
       }
-      console.log(possibleMoves);
       if (board[this.x][this.y + 1]) {
         if (this.x != 0 && board[this.x - 1][this.y + 1] && this.side != board[this.x - 1][this.y + 1].side) {
           possibleMoves.push(`${this.x - 1},${this.y + 1}`);
         }
-        console.log(possibleMoves);
 
         if (this.x != 7 && board[this.x + 1][this.y + 1] && this.side != board[this.x + 1][this.y + 1].side) {
           possibleMoves.push(`${this.x + 1},${this.y + 1}`);
         }
       }
-      console.log(possibleMoves);
     }
     return possibleMoves;
   }
@@ -62,14 +52,3 @@ class Pawn extends Piece {
 }
 
 export default Pawn;
-
-// {
-//   this.y - 1 >= 0 &&
-//     document.getElementById(`${this.x},${this.y - 1}`).innerHTML == '' &&
-//     possibleMoves.push(`${this.x},${this.y - 1}`);
-//   this.y == 6 && possibleMoves.push(`${this.x},${this.y - 2}`);
-//   document.getElementById(`${this.x + 1},${this.y - 1}`).innerHTML != '' &&
-//     possibleMoves.push(`${this.x + 1},${this.y - 1}`);
-//   document.getElementById(`${this.x - 1},${this.y - 1}`).innerHTML != '' &&
-//     possibleMoves.push(`${this.x - 1},${this.y - 1}`);
-// }
