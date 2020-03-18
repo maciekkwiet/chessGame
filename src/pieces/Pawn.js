b94015212f0422fcd52bd5fc909b6347a24cbea1
wbrydak <brydakwojciech@gmail.com>
2020-03-18 16:35:28 +0100

    Ruchy pionków jako odwołanie do boarda

--- src/pieces/Pawn.js
+++ src/pieces/Pawn.js
@@ -6,17 +6,30 @@ class Pawn extends Piece {
     this.name = 'pawn';
     this.display = `<i class="fas fa-chess-pawn ${side}"></i>`;
   }
-  findLegalMoves() {
+  findLegalMoves(board) {
     const possibleMoves = [];
+    console.log(possibleMoves);
+
     if (this.side == 'white') {
-      this.y - 1 >= 0 &&
-        document.getElementById(`${this.x},${this.y - 1}`).innerHTML == '' &&
+      if (!board[this.x][this.y - 1]) {
         possibleMoves.push(`${this.x},${this.y - 1}`);
-      this.y == 6 && possibleMoves.push(`${this.x},${this.y - 2}`);
-      document.getElementById(`${this.x + 1},${this.y - 1}`).innerHTML != '' &&
-        possibleMoves.push(`${this.x + 1},${this.y - 1}`);
-      document.getElementById(`${this.x - 1},${this.y - 1}`).innerHTML != '' &&
-        possibleMoves.push(`${this.x - 1},${this.y - 1}`);
+      }
+      console.log(possibleMoves);
+
+      if (this.y == 6 && !board[this.x][this.y - 2]) {
+        possibleMoves.push(`${this.x},${this.y - 2}`);
+      }
+      console.log(possibleMoves);
+      // bicoe skomentowane bo wypierdala się na ryj XD
+      // if (board[this.x - 1][this.y - 1]) {
+      //   this.x-1 >= 0 && possibleMoves.push(`${this.x - 1}, ${this.y - 1}`);
+      // }
+      // console.log(possibleMoves);
+
+      // if (board[this.x + 1][this.y - 1]) {
+      //   possibleMoves.push(`${this.x + 1}, ${this.y - 1}`);
+      // }
+      // console.log(possibleMoves);
     }
     if (this.side == 'black') {
       this.y + 1 <= 7 &&
@@ -38,3 +51,14 @@ class Pawn extends Piece {
 }
 
 export default Pawn;
+
+// {
+//   this.y - 1 >= 0 &&
+//     document.getElementById(`${this.x},${this.y - 1}`).innerHTML == '' &&
+//     possibleMoves.push(`${this.x},${this.y - 1}`);
+//   this.y == 6 && possibleMoves.push(`${this.x},${this.y - 2}`);
+//   document.getElementById(`${this.x + 1},${this.y - 1}`).innerHTML != '' &&
+//     possibleMoves.push(`${this.x + 1},${this.y - 1}`);
+//   document.getElementById(`${this.x - 1},${this.y - 1}`).innerHTML != '' &&
+//     possibleMoves.push(`${this.x - 1},${this.y - 1}`);
+// }
