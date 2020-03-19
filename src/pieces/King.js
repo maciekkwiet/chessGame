1d7e23d9840f643c54f694b9389909d5eda6c7b1
kacperjanusz <kaacper.janusz@gmail.com>
2020-03-19 19:51:09 +0100

    Third version of King with working oponentMoves

--- src/pieces/King.js
+++ src/pieces/King.js
@@ -7,57 +7,42 @@ class King extends Piece {
     this.display = `<i class="fas fa-chess-king ${side}"></i>`; //fontawesome king
   }
   oponentMoves(gameArea) {
-    console.log("dupa");
-  const oponentMoves2 = [];
+    let oponentMoves2 = [];
 
-for (let i = 0; i <= 7; i++) {
-  for (let j = 0; j <= 7; j++) {
-    if (gameArea[i][j]) {
-      if (gameArea[i][j].side !== this.side ) {
-        console.log("dupa2")
-        
-        console.log(gameArea[i][j].findLegalMoves(gameArea));
- }
-} 
+    for (let i = 0; i <= 7; i++) {
+      for (let j = 0; j <= 7; j++) {
+        if (gameArea[i][j]) {
+          if (gameArea[i][j].side !== this.side && gameArea[i][j].name !== 'king' && gameArea[i][j].name !== 'pawn') {
+            oponentMoves2 = gameArea[i][j].findLegalMoves(gameArea).concat(oponentMoves2);
+          }
+        }
+      }
+    }
+    return oponentMoves2;
   }
-}  
-return oponentMoves2;
-}
-
-
 
   findLegalMoves(gameArea) {
-    const moves = [
-      [1,1],
-      [-1,1],
-      [-1,-1],
-      [1,-1],
-      [1,0],
-      [-1,0],
-      [0,-1],
-      [0,1],
-    ];
+    const moves = [[1, 1], [-1, 1], [-1, -1], [1, -1], [1, 0], [-1, 0], [0, -1], [0, 1]];
+    const oponentKingExists = [[2, 2], [-2, 2], [-2, -2], [2, -2], [2, 0], [-2, 0], [0, -2], [0, 2]];
+    let opoX = 0;
+    let opoY = 0;
     let newX = 0;
     let newY = 0;
     const possibleMoves = [];
-  
-    this.oponentMoves(gameArea);
+
     for (const subTab of moves) {
       newX = this.x + subTab[0];
       newY = this.y + subTab[1];
-<<<<<<< HEAD
-=======
 
->>>>>>> 52c232afff459f4ebe13f08deed1d5d2416d5f06
-     if (newX <= 7 && newX >= 0 && newY <= 7 && newY >= 0) {
-       if (gameArea[newX][newY]) {
-         if (gameArea[newX][newY].side !== this.side) possibleMoves.push(`${newX},${newY}`);
-        }
-      else possibleMoves.push(`${newX},${newY}`);
+      if (newX <= 7 && newX >= 0 && newY <= 7 && newY >= 0) {
+        if (gameArea[newX][newY]) {
+          if (gameArea[newX][newY].side !== this.side) possibleMoves.push(`${newX},${newY}`);
+        } else possibleMoves.push(`${newX},${newY}`);
       }
-    }   
-  return possibleMoves;
+    }
+    let filteredMoves = possibleMoves.filter(move => !this.oponentMoves(gameArea).includes(move));
 
+    return filteredMoves;
   }
 }
 export default King;
