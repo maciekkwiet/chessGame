b64f3e663a02ba75d23795a8ff933da04fe3c3ea
kacperjanusz <kaacper.janusz@gmail.com>
2020-03-19 18:31:28 +0100

    Second version of King

--- src/pieces/King.js
+++ src/pieces/King.js
@@ -6,6 +6,26 @@ class King extends Piece {
     this.name = 'king';
     this.display = `<i class="fas fa-chess-king ${side}"></i>`; //fontawesome king
   }
+  oponentMoves(gameArea) {
+    console.log("dupa");
+  const oponentMoves2 = [];
+
+for (let i = 0; i <= 7; i++) {
+  for (let j = 0; j <= 7; j++) {
+    if (gameArea[i][j]) {
+      if (gameArea[i][j].side !== this.side ) {
+        console.log("dupa2")
+        
+        console.log(gameArea[i][j].findLegalMoves(gameArea));
+ }
+} 
+  }
+}  
+return oponentMoves2;
+}
+
+
+
   findLegalMoves(gameArea) {
     const moves = [
       [1,1],
@@ -20,13 +40,11 @@ class King extends Piece {
     let newX = 0;
     let newY = 0;
     const possibleMoves = [];
+  
+    this.oponentMoves(gameArea);
     for (const subTab of moves) {
       newX = this.x + subTab[0];
       newY = this.y + subTab[1];
-
-
-    
-    return possibleMoves;
      if (newX <= 7 && newX >= 0 && newY <= 7 && newY >= 0) {
        if (gameArea[newX][newY]) {
          if (gameArea[newX][newY].side !== this.side) possibleMoves.push(`${newX},${newY}`);
@@ -38,6 +56,4 @@ class King extends Piece {
 
   }
 }
-
-
 export default King;
