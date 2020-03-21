8ecdc4286c06dba14be3177a542532cd7db4446a
Maciej Kwietniewski <maciek.kwiet36@gmail.com>
2020-03-21 23:36:19 +0100

    first try

--- src/pieces/Bishop.js
+++ src/pieces/Bishop.js
@@ -6,12 +6,12 @@ class Bishop extends Piece {
     this.name = 'bishop';
     this.display = `<i class="fas fa-chess-bishop ${side}"></i>`; //fontawesome bishop
   }
-  findLegalMoves(board) {
-    const possibleMoves = [];
+  findAttackingMoves(board) {
+    const attackingMoves = [];
 
     let leftUp = this.x <= this.y ? this.x : this.y;
     let leftDown = 7 - this.y <= this.x ? 7 - this.y : this.x;
-    let rightUp = this.y <= 7 - this.y ? this.y : 7 - this.x;
+    let rightUp = this.y <= 7 - this.x ? this.y : 7 - this.x;
     let rightDown = 7 - this.y <= 7 - this.x ? 7 - this.y : 7 - this.x;
 
     //console.log(this.x);
@@ -21,63 +21,79 @@ class Bishop extends Piece {
 
     for (let lu = 1; lu <= leftUp; lu++) {
       if (board[this.x - lu][this.y - lu]) {
-        if (board[this.x - lu][this.y - lu].side !== this.side) {
-          possibleMoves.push(`${this.x - lu},${this.y - lu}`);
-          break;
-        }
+        // if (board[this.x - lu][this.y - lu].side !== this.side) {
+        //   possibleMoves.push(`${this.x - lu},${this.y - lu}`);
+        // }
+        attackingMoves.push(`${this.x - lu},${this.y - lu}`);
         break;
       }
-      leftUp > 0 && possibleMoves.push(`${this.x - lu},${this.y - lu}`);
+      leftUp > 0 && attackingMoves.push(`${this.x - lu},${this.y - lu}`);
     }
 
     //  console.log(possibleMoves);
 
     for (let ld = 1; ld <= leftDown; ld++) {
       if (board[this.x - ld][this.y + ld]) {
-        if (board[this.x - ld][this.y + ld].side !== this.side) {
-          possibleMoves.push(`${this.x - ld},${this.y + ld}`);
-          break;
-        }
+        // if (board[this.x - ld][this.y + ld].side !== this.side) {
+        //   possibleMoves.push(`${this.x - ld},${this.y + ld}`);
+        // }
+        attackingMoves.push(`${this.x - ld},${this.y + ld}`);
         break;
       }
-      leftDown > 0 && possibleMoves.push(`${this.x - ld},${this.y + ld}`);
+      leftDown > 0 && attackingMoves.push(`${this.x - ld},${this.y + ld}`);
     }
 
     //  console.log(possibleMoves);
 
     for (let rd = 1; rd <= rightDown; rd++) {
       if (board[this.x + rd][this.y + rd]) {
-        if (board[this.x + rd][this.y + rd].side !== this.side) {
-          possibleMoves.push(`${this.x + rd},${this.y + rd}`);
-          break;
-        }
+        // if (board[this.x + rd][this.y + rd].side !== this.side) {
+        //   possibleMoves.push(`${this.x + rd},${this.y + rd}`);
+        // }
+        attackingMoves.push(`${this.x + rd},${this.y + rd}`);
         break;
       }
-      rightDown > 0 && possibleMoves.push(`${this.x + rd},${this.y + rd}`);
+      rightDown > 0 && attackingMoves.push(`${this.x + rd},${this.y + rd}`);
     }
 
     //  console.log(possibleMoves);
 
     for (let ru = 1; ru <= rightUp; ru++) {
       if (board[this.x + ru][this.y - ru]) {
-        if (board[this.x + ru][this.y - ru].side !== this.side) {
-          possibleMoves.push(`${this.x + ru},${this.y - ru}`);
-          break;
-        }
+        // if (board[this.x + ru][this.y - ru].side !== this.side) {
+        //   possibleMoves.push(`${this.x + ru},${this.y - ru}`);
+        // }
+        attackingMoves.push(`${this.x + ru},${this.y - ru}`);
         break;
       }
-      rightUp > 0 && possibleMoves.push(`${this.x + ru},${this.y - ru}`);
+      rightUp > 0 && attackingMoves.push(`${this.x + ru},${this.y - ru}`);
     }
 
-    // console.log(possibleMoves);
-    // console.log(attackingMoves);
-
-    return possibleMoves;
-  }
-  findAttackingMoves() {
-    const attackingMoves = [];
+    //  console.log(possibleMoves);
 
     return attackingMoves;
+  }
+
+  findLegalMoves(board) {
+    const attack = [];
+    this.findAttackingMoves(board);
+    //console.log(this.findAttackingMoves(board)[0]);
+
+    for (let i = 1; i <= this.findAttackingMoves(board).length; i++) {
+      //console.log(this.findAttackingMoves(board));
+      console.log(this.findAttackingMoves(board)[i - 1]);
+      //var a = this.findAttackingMoves(board);
+      //console.log(a);
+      console.log(board[this.x][this.y]);
+      if (board[i].side === this.side) {
+        attack.push(i);
+        //console.log(attack.push(i));
+      }
+    }
+
+    //console.log(attack);
+
+    return this.findAttackingMoves(board);
   }
 }
 
