2b270f13c09593e6aff836d42803f185896c5993
KlaraGajaszek <klara.gajaszek@student.po.edu.pl>
2020-03-24 01:10:57 +0100

    new function

--- src/pieces/Pawn.js
+++ src/pieces/Pawn.js
@@ -6,46 +6,75 @@ class Pawn extends Piece {
     this.name = 'pawn';
     this.display = `<i class="fas fa-chess-pawn ${side}"></i>`;
   }
-  findLegalMoves(board) {
-    const possibleMoves = [];
+  findAttackingMoves(board) {
+    const attackingMoves = [];
 
     if (this.side == 'white') {
-      if (!board[this.x][this.y - 1]) {
-        possibleMoves.push(`${this.x},${this.y - 1}`);
+      if (!board[this.x][this.y - 1]) {//ruch na przód
+        attackingMoves.push(`${this.x},${this.y - 1}`);
+      }
+      console.log(board)
+
+      if (this.y == 6 && !board[this.x][this.y - 1] && !board[this.x]/*ruch o dwa do przodu z pozycji  y=6*/[this.y - 2]) {
+        attackingMoves.push(`${this.x},${this.y - 2}`);
       }
 
-      if (this.y == 6 && !board[this.x][this.y - 1] && !board[this.x][this.y - 2]) {
-        possibleMoves.push(`${this.x},${this.y - 2}`);
+      if (this.x != 0 && board[this.x - 1][this.y - 1] /*&& this.side != board[this.x - 1][this.y - 1].side*/ ) {
+        attackingMoves.push(`${this.x - 1},${this.y - 1}`);
       }
 
-      if (this.x != 0 && board[this.x - 1][this.y - 1] && this.side != board[this.x - 1][this.y - 1].side) {
-        possibleMoves.push(`${this.x - 1},${this.y - 1}`);
-      }
-
-      if (this.x != 7 && board[this.x + 1][this.y - 1] && this.side != board[this.x + 1][this.y - 1].side) {
-        possibleMoves.push(`${this.x + 1},${this.y - 1}`);
+      if (this.x != 7 && board[this.x + 1][this.y - 1] /*&& this.side != board[this.x + 1][this.y - 1].side*/) {
+        attackingMoves.push(`${this.x + 1},${this.y - 1}`);
       }
     }
     if (this.side == 'black') {
       if (!board[this.x][this.y + 1]) {
-        possibleMoves.push(`${this.x},${this.y + 1}`);
+        attackingMoves.push(`${this.x},${this.y + 1}`);
       }
 
       if (this.y == 1 && !board[this.x][this.y + 1] && !board[this.x][this.y + 2]) {
-        possibleMoves.push(`${this.x},${this.y + 2}`);
+        attackingMoves.push(`${this.x},${this.y + 2}`);
       }
-      if (this.x != 0 && board[this.x - 1][this.y + 1] && this.side != board[this.x - 1][this.y + 1].side) {
-        possibleMoves.push(`${this.x - 1},${this.y + 1}`);
+      if (this.x != 0 && board[this.x - 1][this.y + 1] /*&& this.side != board[this.x - 1][this.y + 1].side*/) {
+        attackingMoves.push(`${this.x - 1},${this.y + 1}`);
       }
 
-      if (this.x != 7 && board[this.x + 1][this.y + 1] && this.side != board[this.x + 1][this.y + 1].side) {
-        possibleMoves.push(`${this.x + 1},${this.y + 1}`);
+      if (this.x != 7 && board[this.x + 1][this.y + 1] /*&& this.side != board[this.x + 1][this.y + 1].side*/) {
+        attackingMoves.push(`${this.x + 1},${this.y + 1}`);
       }
     }
-    return possibleMoves;
+  
+    return attackingMoves;
   }
-  promote() {}
-  enPassant() {}
-}
 
+    findLegalMoves(board)
+    {
+      let attack=this.findAttackingMoves(board);
+      let legal=[];
+      console.log("attack: " + attack);
+      
+     for(const move of attack)
+      {
+       const attackX=move[0];
+       const attackY=move[2];
+       //console.log("X: "+attackX);
+       //console.log("Y: "+attackY);
+
+      
+       if (!board[attackX][attackY] || board[attackX][attackY].side !=this.side)
+       {
+        legal.push(move);
+       }
+       
+      
+      
+    }
+    console.log("legal: "+legal);
+    return legal;
+ 
+}
+//promote() {}
+//enPassant() {}
+
+}
 export default Pawn;
