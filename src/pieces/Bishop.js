5ae8b3f6822a87d21cf9f46fb82e8226d2f23a61
Maciej Kwietniewski <maciek.kwiet36@gmail.com>
2020-03-24 14:45:57 +0100

    finish attacking function

--- src/pieces/Bishop.js
+++ src/pieces/Bishop.js
@@ -14,91 +14,56 @@ class Bishop extends Piece {
     let rightUp = this.y <= 7 - this.x ? this.y : 7 - this.x;
     let rightDown = 7 - this.y <= 7 - this.x ? 7 - this.y : 7 - this.x;
 
-    //console.log(this.x);
-    //console.log(this.y);
-    //console.log(board);
-    //console.log(board[this.x][this.y]);
-
     for (let lu = 1; lu <= leftUp; lu++) {
       if (board[this.x - lu][this.y - lu]) {
-        // if (board[this.x - lu][this.y - lu].side !== this.side) {
-        //   possibleMoves.push(`${this.x - lu},${this.y - lu}`);
-        // }
         attackingMoves.push(`${this.x - lu},${this.y - lu}`);
         break;
       }
       leftUp > 0 && attackingMoves.push(`${this.x - lu},${this.y - lu}`);
     }
 
-    //  console.log(possibleMoves);
-
     for (let ld = 1; ld <= leftDown; ld++) {
       if (board[this.x - ld][this.y + ld]) {
-        // if (board[this.x - ld][this.y + ld].side !== this.side) {
-        //   possibleMoves.push(`${this.x - ld},${this.y + ld}`);
-        // }
         attackingMoves.push(`${this.x - ld},${this.y + ld}`);
         break;
       }
       leftDown > 0 && attackingMoves.push(`${this.x - ld},${this.y + ld}`);
     }
 
-    //  console.log(possibleMoves);
-
     for (let rd = 1; rd <= rightDown; rd++) {
       if (board[this.x + rd][this.y + rd]) {
-        // if (board[this.x + rd][this.y + rd].side !== this.side) {
-        //   possibleMoves.push(`${this.x + rd},${this.y + rd}`);
-        // }
         attackingMoves.push(`${this.x + rd},${this.y + rd}`);
         break;
       }
       rightDown > 0 && attackingMoves.push(`${this.x + rd},${this.y + rd}`);
     }
 
-    //  console.log(possibleMoves);
-
     for (let ru = 1; ru <= rightUp; ru++) {
       if (board[this.x + ru][this.y - ru]) {
-        // if (board[this.x + ru][this.y - ru].side !== this.side) {
-        //   possibleMoves.push(`${this.x + ru},${this.y - ru}`);
-        // }
         attackingMoves.push(`${this.x + ru},${this.y - ru}`);
         break;
       }
       rightUp > 0 && attackingMoves.push(`${this.x + ru},${this.y - ru}`);
     }
 
-    //  console.log(possibleMoves);
-
     return attackingMoves;
   }
 
   findLegalMoves(board) {
     const attack = [];
-    const a = this.findAttackingMoves(board);
+    const param = this.findAttackingMoves(board);
 
-    for (let i = 1; i <= a.length; i++) {
-      const tab = [...a[i - 1]];
-
-      //console.log(a[i - 1]);
-
-      //console.log(this.side);
-      //console.log(board[tab[0]][tab[2]].side);
-      //console.log(board[tab[0]][tab[2]]);
+    for (let i = 0; i < param.length; i++) {
+      const tab = param[i];
 
       if (board[tab[0]][tab[2]]) {
         if (board[tab[0]][tab[2]].side == this.side) {
-          attack.push(a[i - 1]);
+          attack.push(param[i]);
         }
       }
     }
 
-    //console.log(attack);
-
-    let filteredMoves = a.filter(move => !attack.includes(move));
-
-    // od a odjać attack
+    const filteredMoves = param.filter(move => !attack.includes(move));
 
     return filteredMoves;
   }
