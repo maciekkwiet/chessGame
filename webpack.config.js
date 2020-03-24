c590ec0fb0a02beec67ab0bb419fe232d0b80d7e
wbrydak <brydakwojciech@gmail.com>
2020-03-24 21:04:02 +0100

    w końcu jakoś wygląda

--- webpack.config.js
+++ webpack.config.js
@@ -41,6 +41,18 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.(woff(2)?|otf|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/',
+            },
+          },
+        ],
+      },
     ],
   },
 };
