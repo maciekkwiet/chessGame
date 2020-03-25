561fbcb7e6752479f322e2ea8efb99d3df2ceb22
KlaraGajaszek <klara.gajaszek@student.po.edu.pl>
2020-03-25 15:45:06 +0100

    add new function

--- src/pieces/Pawn.js
+++ src/pieces/Pawn.js
@@ -10,71 +10,77 @@ class Pawn extends Piece {
     const attackingMoves = [];
 
     if (this.side == 'white') {
-      if (!board[this.x][this.y - 1]) {//ruch na przód
-        attackingMoves.push(`${this.x},${this.y - 1}`);
-      }
-      console.log(board)
-
-      if (this.y == 6 && !board[this.x][this.y - 1] && !board[this.x]/*ruch o dwa do przodu z pozycji  y=6*/[this.y - 2]) {
-        attackingMoves.push(`${this.x},${this.y - 2}`);
-      }
-
-      if (this.x != 0 && board[this.x - 1][this.y - 1] /*&& this.side != board[this.x - 1][this.y - 1].side*/ ) {
+      if (this.x != 0 ) {
         attackingMoves.push(`${this.x - 1},${this.y - 1}`);
       }
 
-      if (this.x != 7 && board[this.x + 1][this.y - 1] /*&& this.side != board[this.x + 1][this.y - 1].side*/) {
-        attackingMoves.push(`${this.x + 1},${this.y - 1}`);
-      }
+      if (this.x != 7 ) {
+        attackingMoves.push(`${this.x + 1},${this.y - 1}`);       
+      }  
     }
-    if (this.side == 'black') {
-      if (!board[this.x][this.y + 1]) {
-        attackingMoves.push(`${this.x},${this.y + 1}`);
-      }
+      if (this.side == 'black') {
 
-      if (this.y == 1 && !board[this.x][this.y + 1] && !board[this.x][this.y + 2]) {
-        attackingMoves.push(`${this.x},${this.y + 2}`);
-      }
-      if (this.x != 0 && board[this.x - 1][this.y + 1] /*&& this.side != board[this.x - 1][this.y + 1].side*/) {
+      if (this.x != 0 ) {
         attackingMoves.push(`${this.x - 1},${this.y + 1}`);
       }
 
-      if (this.x != 7 && board[this.x + 1][this.y + 1] /*&& this.side != board[this.x + 1][this.y + 1].side*/) {
+      if (this.x != 7 ) {
         attackingMoves.push(`${this.x + 1},${this.y + 1}`);
       }
+
     }
-  
+      console.log("attack " + attackingMoves)
     return attackingMoves;
   }
 
-    findLegalMoves(board)
-    {
-      let attack=this.findAttackingMoves(board);
-      let legal=[];
-      console.log("attack: " + attack);
-      
-     for(const move of attack)
-      {
-       const attackX=move[0];
-       const attackY=move[2];
-       //console.log("X: "+attackX);
-       //console.log("Y: "+attackY);
+  findLegalMoves(board)
+{
+  let attack=this.findAttackingMoves(board);
+  const legalMoves=[];
 
-      
-       if (!board[attackX][attackY] || board[attackX][attackY].side !=this.side)
-       {
-        legal.push(move);
-       }
-       
-      
-      
+
+  if (this.side == 'white') {
+    if (!board[this.x][this.y - 1]) {//ruch na przód
+      legalMoves.push(`${this.x},${this.y - 1}`);
     }
-    console.log("legal: "+legal);
-    return legal;
- 
+    console.log(board)
+
+    if (this.y == 6 && !board[this.x][this.y - 1]  && !board[this.x][this.y - 2]) { /*ruch o dwa do przodu z pozycji  y=6*/
+      legalMoves.push(`${this.x},${this.y - 2}`);
+    }
+
+     if (this.x != 0 && board[this.x - 1][this.y - 1]  && this.side != board[this.x - 1][this.y - 1].side ) {
+      legalMoves.push(`${this.x - 1},${this.y - 1}`);
+    }
+
+    if (this.x != 7 && board[this.x + 1][this.y - 1] && this.side != board[this.x + 1][this.y - 1].side) {
+      legalMoves.push(`${this.x + 1},${this.y - 1}`);
+    }
+  }
+  if (this.side == 'black') {
+    if (!board[this.x][this.y + 1]) {
+      legalMoves.push(`${this.x},${this.y + 1}`);
+    }
+
+    if (this.y == 1 && !board[this.x][this.y + 1] && !board[this.x][this.y + 2]) {
+      legalMoves.push(`${this.x},${this.y + 2}`);
+    }
+    if (this.x != 0 && board[this.x - 1][this.y + 1] && this.side != board[this.x - 1][this.y + 1].side) {
+      legalMoves.push(`${this.x - 1},${this.y + 1}`);
+    }
+
+    if (this.x != 7 && board[this.x + 1][this.y + 1]  &&this.side != board[this.x + 1][this.y + 1].side) {
+      legalMoves.push(`${this.x + 1},${this.y + 1}`);
+    }
+
+  }  
+  //console.log("legal: " + legalMoves)
+  return legalMoves;
 }
-//promote() {}
-//enPassant() {}
+
+
+promote() {}
+enPassant() {}
 
 }
 export default Pawn;
