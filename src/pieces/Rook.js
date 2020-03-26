46b30055ffed0382c8454c5f23a10de4a078d4b7
Maciej Kwietniewski <maciek.kwiet36@gmail.com>
2020-03-26 17:46:47 +0100

    master with rook

--- src/pieces/Rook.js
+++ src/pieces/Rook.js
@@ -1,5 +1,4 @@
-
-import Piece from './Piece';
+import Piece from './piece';
 
 class Rook extends Piece {
   constructor(x, y, side) {
@@ -7,49 +6,57 @@ class Rook extends Piece {
     this.name = 'rook';
     this.display = `<i class="fas fa-chess-rook ${side}"></i>`; //fontawesome rook
   }
+
   findLegalMoves(board) {
     const possibleMoves = [];
+    const attackingMoves = this.findAttackingMoves(board);
+    console.log('Rook -> findLegalMoves -> attackingMoves', attackingMoves);
+    for (let move of attackingMoves) {
+      if (board[move.charAt(0)][move.charAt(2)] && board[move.charAt(0)][move.charAt(2)].side === this.side) continue;
+      possibleMoves.push(move);
+    }
+    return possibleMoves;
+  }
+
+  findAttackingMoves(board) {
+    const attackingMoves = [];
 
     for (let u = 1; u <= 7; u++) {
       if (this.y - u < 0) break;
       if (board[this.x][this.y - u]) {
-        if (board[this.x][this.y - u].side !== this.side) possibleMoves.push(`${this.x},${this.y - u}`);
+        if (board[this.x][this.y - u]) attackingMoves.push(`${this.x},${this.y - u}`);
         break;
       }
-      possibleMoves.push(`${this.x},${this.y - u}`);
+      attackingMoves.push(`${this.x},${this.y - u}`);
     }
 
     for (let d = 1; d <= 7; d++) {
       if (this.y + d > 7) break;
       if (board[this.x][this.y + d]) {
-        if (board[this.x][this.y + d].side !== this.side) possibleMoves.push(`${this.x},${this.y + d}`);
+        if (board[this.x][this.y + d]) attackingMoves.push(`${this.x},${this.y + d}`);
         break;
       }
-      possibleMoves.push(`${this.x},${this.y + d}`);
+      attackingMoves.push(`${this.x},${this.y + d}`);
     }
 
     for (let l = 1; l <= 7; l++) {
       if (this.x - l < 0) break;
       if (board[this.x - l][this.y]) {
-        if (board[this.x - l][this.y].side !== this.side) possibleMoves.push(`${this.x - l},${this.y}`);
+        if (board[this.x - l][this.y]) attackingMoves.push(`${this.x - l},${this.y}`);
         break;
       }
-      possibleMoves.push(`${this.x - l},${this.y}`);
+      attackingMoves.push(`${this.x - l},${this.y}`);
     }
 
     for (let r = 1; r <= 7; r++) {
       if (this.x + r > 7) break;
       if (board[this.x + r][this.y]) {
-        if (board[this.x + r][this.y].side !== this.side) possibleMoves.push(`${this.x + r},${this.y}`);
+        if (board[this.x + r][this.y]) attackingMoves.push(`${this.x + r},${this.y}`);
         break;
       }
-      possibleMoves.push(`${this.x + r},${this.y}`);
+      attackingMoves.push(`${this.x + r},${this.y}`);
     }
-
-    return possibleMoves;
+    return attackingMoves;
   }
 }
-
 export default Rook;
-
-
