68863209a1d34f90155bd2892c411fb0543ac847
wbrydak <brydakwojciech@gmail.com>
2020-03-28 21:22:39 +0100

    Fixed overlay deactivation

--- src/utils.js
+++ src/utils.js
@@ -15,3 +15,14 @@ export const handleOverlay = () =>
     document.getElementById('startScreen').style.transition = 'opacity 0.3s linear';
   });
 ;
+export const handleOverlay = () =>
+  document.getElementById('play').addEventListener('click', function() {
+    document.getElementById('startScreen').style.animationPlayState = 'paused';
+    document.getElementById('startScreen').style.opacity = '0';
+    document.getElementById('startScreen').style.transition = 'opacity 0.3s linear';
+    const isFinished = document.getElementById('startScreen').style.transition;
+    console.log(isFinished);
+    if (isFinished === 'opacity 0.3s linear 0s') {
+      document.getElementById('startScreen').style.display = 'none';
+    }
+  });
