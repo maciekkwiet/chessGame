b78375a1db8712408fa985a8d56e3f28951c8217
wbrydak <brydakwojciech@gmail.com>
2020-04-01 13:08:50 +0200

    handled overlay animation

--- src/utils.js
+++ src/utils.js
@@ -26,12 +26,5 @@ export const parseId = id => [Number(id[0]), Number(id[2])];
 
 export const handleOverlay = () =>
   document.getElementById('play').addEventListener('click', function() {
-    document.getElementById('startScreen').style.animationPlayState = 'paused';
-    document.getElementById('startScreen').style.opacity = '0';
-    document.getElementById('startScreen').style.transition = 'opacity 0.3s linear';
-    const isFinished = document.getElementById('startScreen').style.transition;
-    console.log(isFinished);
-    if (isFinished === 'opacity 0.3s linear 0s') {
-      document.getElementById('startScreen').style.display = 'none';
-    }
+    document.getElementById('startScreen').style.animationPlayState = 'running';
   });
