40b4ad31523a51c899460a03e94be47d715745bd
CaptainObjective <marcinwos18@gmail.com>
2020-04-06 20:58:43 +0200

    wskazówki

--- src/Game.js
+++ src/Game.js
@@ -10,17 +10,14 @@ class Game {
     this.gameArea = this.board.gameArea;
     this.legalMoves = [];
     this.selectedPiece = null;
-    this.board.gameAreaHandler.addEventListener('click', e => this.onClick(e));    
-    this.whiteplayer = new TIME(900,"timerwhite");
-    this.blackplayer = new TIME(900,"timerblack");    
-    
+    this.board.gameAreaHandler.addEventListener('click', e => this.onClick(e));
+    this.whiteplayer = new TIME(900, 'timerwhite');
+    this.blackplayer = new TIME(900, 'timerblack');
   }
 
-
   onClick(e) {
-   
     const element = e.target.classList.contains('square') ? e.target : e.target.parentElement;
-   
+
     if (this.legalMoves.length !== 0) {
       this.handleMove(element);
     } else {
@@ -34,47 +31,46 @@ class Game {
     this.round++;
   }
 
-  changecolor(x,y)
-  {    
-      const currentColorPlayer=document.getElementById(x,y);
+  // Ta metoda powinna istnieć w klasie board, w game nie robimy interakcji z DOM, ponadto Maciek pisał podobną metode można by ją wykorzystać
+  changecolor(x, y) {
+    // y jest zawsze undefined ponieważ przyjmujesz tutaj dwa parametry a przekazujesz tylko jeden
+    const currentColorPlayer = document.getElementById(x, y);
 
-      if(currentColorPlayer.className != "currentcolor square")
-      {
-        currentColorPlayer.className = 'currentcolor square'
-      }
-      else
-      {
-        y % 2 == x % 2 ? currentColorPlayer.className='square light' : currentColorPlayer.className='square dark'; 
-      }     
-      return x,y;
+    //polecam zapoznać się z add, remove i toggle znaczenie uprości ten kod https://developer.mozilla.org/pl/docs/Web/API/Element/classList
+    if (currentColorPlayer.className != 'currentcolor square') {
+      currentColorPlayer.className = 'currentcolor square';
+    } else {
+      y % 2 == x % 2 ? (currentColorPlayer.className = 'square light') : (currentColorPlayer.className = 'square dark');
+    }
+    return x, y; //funkcje w js (i nie tylko) nie mogą zwracać więcej niż jednego elementu
   }
 
- backchangecolor()
-  {
-    this.changecolor();
-    const backx =this.changecolor.x;
-    const backy=this.changecolor.y;
-    const currentColorPlayerback=document.getElementById(backx,backy);
-    
-    if(currentColorPlayerback.className == 'currentcolor square')
-      {
-        backy % 2 == backx % 2 ? currentColorPlayerback.className='square light' : currentColorPlayerback.className='square dark'; 
-      }   
-   
+  // Ta metoda powinna istnieć w klasie board, choć bardzo możliwe że okaże się nie potrzebna
+  // Obczaj metode toggle: https://developer.mozilla.org/pl/docs/Web/API/Element/classList
+  backchangecolor() {
+    this.changecolor(); //wywołałaś funkcję więc cokolwiek ona zwróciła jest dostępne w tej lini i tylko w tej linii
+    // szukasz właściwości x na funkcji (nie na wartości zwróconej przez funkcje). Jako że funkcje są obiektami to jest to możliwe aczkolwiek tutaj nie ma to sensu
+    const backx = this.changecolor.x;
+    const backy = this.changecolor.y;
+
+    const currentColorPlayerback = document.getElementById(backx, backy);
+
+    if (currentColorPlayerback.className == 'currentcolor square') {
+      backy % 2 == backx % 2
+        ? (currentColorPlayerback.className = 'square light')
+        : (currentColorPlayerback.className = 'square dark');
+    }
   }
 
   handleSelect(element) {
     const [x, y] = parseId(element.id);
-   
+
     if (!this.gameArea[x][y] || this.gameArea[x][y].side !== this.currentPlayer) return;
-     
-    
-      this.changecolor(`${x},${y}`)
-        
-    
+
+    this.changecolor(`${x},${y}`);
+
     this.selectedPiece = this.gameArea[x][y];
     const possibleMoves = this.selectedPiece.findLegalMoves(this.gameArea);
-    
 
     // ToDo refactor
     if (this.selectedPiece.name === 'king' && !this.isChecked())
@@ -89,22 +85,20 @@ class Game {
   handleMove(element) {
     //const [x, y] = parseId(element.id);
     const { id } = element;
-    console.log({ id })
-    this.backchangecolor()
+    console.log({ id });
+    this.backchangecolor();
     if (!this.legalMoves.includes(id)) return;
-      
+
     // ToDo refactor
     if (this.selectedPiece.name === 'king' && Math.abs(this.selectedPiece.x - id[0]) > 1) {
       this.selectedPiece.castling(this.gameArea, parseId(id));
     } else this.board.movePiece(this.selectedPiece, parseId(id));
 
-    if(this.currentPlayer =='white') this.whiteplayer.start(); 
+    if (this.currentPlayer == 'white') this.whiteplayer.start();
     else this.whiteplayer.pause();
-   if(this.currentPlayer =='black') this.blackplayer.start(); 
+    if (this.currentPlayer == 'black') this.blackplayer.start();
     else this.blackplayer.pause();
 
-
-   
     // ToDo refactor
     if (this.selectedPiece.name === 'pawn') {
       if (
